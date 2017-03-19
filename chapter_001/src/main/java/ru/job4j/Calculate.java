473d87aaaa2b324fa64bf160c76f5ec7627d583f
Anastasia Gladun <netmislei@mail.ru>
2017-03-19 20:45:06 +0800

    Calculate

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,16 @@
-package ru.job4j;
+﻿package ru.job4j;
 
 /**
 *Class Calculate решение задачи части 001 урок1.
 *@author Anastasia Gladun
+*@version 0.2
 *@since 19.03.2017
 */
 public class Calculate{
 	/**
 	*Метод выводит в консоль текст "Hello world." 
 	*@param args массив строковых аргументов
-	*@version 1
+	*@since 0.1 
 	*/
 	public static void main(String[] args){
 		System.out.println("Hello world.");
