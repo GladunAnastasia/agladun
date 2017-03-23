0161aa6bbd9a6925e11dad5318f166f1453530e4
Anastasia Gladun <netmislei@mail.ru>
2017-03-24 01:01:02 +0800

    new

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -13,11 +13,14 @@ import static org.hamcrest.core.Is.is;
  */
 public class MaxTest {
 	/**
+	 * max.
+	 */
+	private Max max = new Max();
+	/**
 	 * MaxTest.
 	 */
 	@Test
 	public void whenMaxTwelveMoreThreeThenTwelve() {
-		Max max = new Max();
 		int result = max.max(12, 3);
 		assertThat(result, is(12));
 	}
@@ -26,7 +29,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxThreeLessTwelveThenTwelve() {
-		Max max = new Max();
 		int result = max.max(3, 12);
 		assertThat(result, is(12));
 	}
@@ -35,7 +37,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxOneTwoThreeThenTwelve() {
-		Max max = new Max();
 		int result = max.max(1, 2, 3);
 		assertThat(result, is(3));
 	}
@@ -44,7 +45,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxTwoOneThreeThenTwelve() {
-		Max max = new Max();
 		int result = max.max(2, 1, 3);
 		assertThat(result, is(3));
 	}
@@ -53,7 +53,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxTwoThreeOneThenTwelve() {
-		Max max = new Max();
 		int result = max.max(2, 3, 1);
 		assertThat(result, is(3));
 	}
@@ -62,7 +61,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxOneThreeTwoThenTwelve() {
-		Max max = new Max();
 		int result = max.max(1, 3, 2);
 		assertThat(result, is(3));
 	}
@@ -71,7 +69,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxThreeOneTwoThenTwelve() {
-		Max max = new Max();
 		int result = max.max(3, 1, 2);
 		assertThat(result, is(3));
 	}
@@ -80,7 +77,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxThreeTwoOneThenTwelve() {
-		Max max = new Max();
 		int result = max.max(3, 2, 1);
 		assertThat(result, is(3));
 	}
@@ -89,7 +85,6 @@ public class MaxTest {
 	 */
 	@Test
 	public void whenMaxThreeThreeThreeThenTwelve() {
-		Max max = new Max();
 		int result = max.max(3, 3, 3);
 		assertThat(result, is(3));
 	}
