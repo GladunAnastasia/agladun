2a46b629885d89fc44cbe33d219f4561aa6490c2
AnastasiaGladun <netmislei@mail.ru>
2017-04-07 20:25:09 +0800

    new

---
+++ chapter_002/src/test/java/ru.job4j/tracker/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package.
+ * @author Анастасия Гладун (netmislei@mail.ru)
+ * @version 0.1
+ * @since 0.1
+ */
+package ru.job4j.tracker;
---
+++ testTask/Contains.java
@@ -0,0 +1,17 @@
+﻿public class Contains {
+    public static void main(String[] args) {
+        Contains con = new Contains();
+        String origin = "длиннаястрокаоченьдлинная";
+        String sub = "строка";
+        System.out.println(con.contains(origin, sub));
+    }
+    public boolean contains(String origin, String sub) {
+        char[] originChar = origin.toCharArray();
+        char[] subChar = sub.toCharArray();
+        int j = 0;
+        for (int i = 0; j < subChar.length && i < originChar.length; i++) {
+            j = (j != subChar.length && originChar[i] == subChar[j]) ? j + 1 : 0;
+        }
+        return j == subChar.length;
+    }
+}
