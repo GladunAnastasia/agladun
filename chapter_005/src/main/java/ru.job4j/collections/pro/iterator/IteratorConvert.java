3b864f538b00bbc4739d1c9cd7e8a86c071eec37
Anastasia Gladun <netmislei@mail.ru>
2017-05-06 21:47:31 +0800

    new

---
+++ chapter_005/src/main/java/ru.job4j/collections/pro/iterator/IteratorConvert.java
@@ -0,0 +1,60 @@
+package ru.job4j.collections.pro.iterator;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+/**
+ * Класс IteratorConvert.
+ *
+ * @author Анастасия Гладун (netmislei@mail.ru)
+ * @since 06.05.2017
+ */
+public class IteratorConvert implements Iterator<Integer> {
+    /**
+     * Итератор итераторов.
+     */
+    private Iterator<Iterator<Integer>> itIterator;
+    /**
+     * Итератор.
+     */
+    private Iterator<Integer> itInteger;
+
+    /**
+     * Проверяет существование следующего элемента итератора или списка.
+     *
+     * @return - возвращает true или false.
+     */
+    @Override
+    public boolean hasNext() {
+        return itInteger.hasNext();
+    }
+
+    /**
+     * @return - возвращает следующий элемент массива.
+     */
+    @Override
+    public Integer next() {
+        return itInteger.hasNext() ? itInteger.next() : null;
+    }
+
+    /**
+     * Ковертирует итератор итераторов, которвые содержат данные типа Integer, в один итератор со всеми данными типа Integer.
+     *
+     * @param it - Ковертирует итератор итераторов, которвые содержат данные типа Integer.
+     * @return - возвращает итератор, содержащий данные типа Integer.
+     */
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        itIterator = it;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        while (itIterator.hasNext()) {
+            itInteger = itIterator.next();
+            while (hasNext()) {
+                list.add(next());
+            }
+        }
+        return list.iterator();
+    }
+
+
+}
---
+++ chapter_005/src/test/java/ru.job4j/collections/pro/iterator/IteratorConvertTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.collections.pro.iterator;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Класс IteratorConvert.
+ *
+ * @author Анастасия Гладун (netmislei@mail.ru)
+ * @since 06.05.2017
+ */
+public class IteratorConvertTest {
+    /**
+     * Test method.
+     */
+    @Test
+    public void whenIteratorIteratorsConvertsIteratorThenIterator() {
+        IteratorConvert itConvert = new IteratorConvert();
+        List<Integer> list1 = Arrays.asList(new Integer[]{1, 2, 3, 4});
+        List<Integer> list2 = Arrays.asList(new Integer[]{5, 6, 7});
+        List<Integer> list3 = Arrays.asList(new Integer[]{8, 9, 10, 11});
+        List<Iterator<Integer>> list = Arrays.asList(new Iterator[]{list1.iterator(), list2.iterator(), list3.iterator()});
+        Iterator<Iterator<Integer>> itit = list.iterator();
+        Iterator<Integer> it = (Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)).iterator();
+        Iterator<Integer> it2 = itConvert.convert(itit);
+        while (it.hasNext() || it2.hasNext()) {
+            assertThat(it.next(), is(it2.next()));
+        }
+    }
+}
