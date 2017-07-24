026f3f533899771303408ed972f8e1ab5c330fc7
Anastasia Gladun <netmislei@mail.ru>
2017-07-24 13:47:45 +0300

    new

--- chapter_005/src/test/java/ru/job4j/collections/pro/set/ImprovedSetTest.java
+++ chapter_005/src/test/java/ru/job4j/collections/pro/set/ImprovedSetTest.java
@@ -18,7 +18,7 @@ public class ImprovedSetTest {
      * Test method.
      */
     @Test
-    public void whenCompareTwoMethodsThen() {
+    public void whenCompareTwoMethodsThenFirstIsFaster() {
         Date date1 = new Date();
         ImprovedSet<Integer> set = new ImprovedSet<>();
         for (int i = 0; i < 100000; i++) {
@@ -28,17 +28,15 @@ public class ImprovedSetTest {
             set.add(i);
         }
         Date date2 = new Date();
-        //System.out.println(date2.getTime() - date1.getTime());
-        Date date3 = new Date();
         MySetLinkedList<Integer> listSet = new MySetLinkedList<>();
         for (int i = 0; i < 100000; i++) {
             listSet.add(i);
         }
         for (int i = 0; i < 80000; i++) {
-            set.add(i);
+            listSet.add(i);
         }
-        Date date4 = new Date();
-        System.out.println((date4.getTime() - date3.getTime()) + " больше, чем " + (date2.getTime() - date1.getTime()));
+        Date date3 = new Date();
+        System.out.println((date2.getTime() - date1.getTime()) + " меньше, чем " + (date3.getTime() - date2.getTime()));
         assertThat(set.size(), is(listSet.getLength()));
     }
 }
