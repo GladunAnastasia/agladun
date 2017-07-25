9ad18dc54088f1124b498b20362f10a128383d70
Anastasia Gladun <netmislei@mail.ru>
2017-07-26 00:29:06 +0300

    new

--- chapter_005/src/main/java/ru.job4j/collections/pro/map/User1.java
+++ chapter_005/src/main/java/ru.job4j/collections/pro/map/User1.java
@@ -3,28 +3,16 @@ package ru.job4j.collections.pro.map;
 import java.util.Calendar;
 
 /**
-<<<<<<< HEAD
  * Класс User1.
-=======
- * Класс User.
->>>>>>> 58d5c0b... new
  *
  * @author Анастасия Гладун
  * @since 24.07.2017
  */
-<<<<<<< HEAD
 public class User1 {
     private String name;
     private int children;
     private Calendar birthday;
     public User1(String name, int children, Calendar birthday) {
-=======
-public class User {
-    private String name;
-    private int children;
-    private Calendar birthday;
-    public User(String name, int children, Calendar birthday) {
->>>>>>> 58d5c0b... new
         this.name = name;
         this.children = children;
         this.birthday = birthday;
