51225a727335618d2d8dc91dfa6abed0ef66265d
Anastasia Gladun <netmislei@mail.ru>
2017-07-26 23:01:47 +0300

    5. Перекрывать и equals и hashCode[#20101]

---
+++ chapter_005/src/main/java/ru.job4j/collections/pro/map/User4.java
@@ -0,0 +1,47 @@
+package ru.job4j.collections.pro.map;
+
+import java.util.Calendar;
+
+/**
+ * Класс User4.
+ *
+ * @author Анастасия Гладун
+ * @since 24.07.2017
+ */
+public class User4 {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User4(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || o.getClass() != getClass()) {
+            return false;
+        }
+        User4 user = (User4) o;
+        if (name != null ? !name.equals(user.name) : user.name != null) {
+            return false;
+        }
+        if (children != user.children) {
+            return false;
+        }
+        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
+}
