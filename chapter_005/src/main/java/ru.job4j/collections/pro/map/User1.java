c4c82489f567a58955f709d7987eba3d1bdf2292
Anastasia Gladun <netmislei@mail.ru>
2017-07-26 22:55:17 +0300

    1. Создать модель User[#20096]

---
+++ chapter_005/src/main/java/ru.job4j/collections/pro/map/User1.java
@@ -0,0 +1,20 @@
+package ru.job4j.collections.pro.map;
+
+import java.util.Calendar;
+
+/**
+ * Класс User1.
+ *
+ * @author Анастасия Гладун
+ * @since 24.07.2017
+ */
+public class User1 {
+    private String name;
+    private int children;
+    private Calendar birthday;
+    public User1(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
